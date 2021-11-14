6e0201b0d7a8e13b0bcff006186b6e838caa0871
nvjome <nvjome@gmail.com>
2021-11-14 12:53:28 -0600

    Added example sketches

---
+++ examples/LEDandButton_1/LEDandButton_1.ino
@@ -0,0 +1,41 @@
+/*
+ * Example sketch of using the DebounceNoInt library.
+ * https://github.com/nvjome/DebounceNoInt
+ * 
+ * Turns on LED when a button is pressed, turns off when released.
+ * Uses DEBOUNCE_RISE and DEBOUNCE_FALL states to detect button and control LED.
+ * 
+ * Written by nvjome.
+ */
+
+#include <DebounceNoInt.h>
+
+#define LED_PIN LED_BUILTIN
+#define BUTTON_PIN 4
+
+// create debounce object
+// attach to button pin, buton active low, 5ms debounce time, internal pullup active (true)
+DebounceNoInt button(BUTTON_PIN, DEBOUNCE_ACTIVE_LOW, 5000, true);
+
+void setup() {
+  // initialize object
+  button.begin();
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+}
+
+void loop() {
+  // update debounce state
+  button.run();
+
+  // get debounced input state and light LED accordingly
+  if (button.getDebounceState() == DEBOUNCE_FALL) {
+    // button pressed
+    digitalWrite(LED_PIN, HIGH);
+  }
+  if (button.getDebounceState() == DEBOUNCE_RISE){
+    // button released
+    digitalWrite(LED_PIN, LOW);
+  }
+}
