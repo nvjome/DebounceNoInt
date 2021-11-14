fe9bc8bce4e0db1d169968fe32b8bcf17b2f683f
nvjome <nvjome@gmail.com>
2021-11-13 21:12:53 -0600

    Update DebounceNoInt.h

--- DebounceNoInt.h
+++ DebounceNoInt.h
@@ -3,12 +3,14 @@
 
 class DebounceNoInt {
 	public:
-		
-	
+		debounceNoInt(int pin, uint8_t activeHL, unsigned long interval);
+		virtual ~DebounceNoInt();
 	
 	
 	private:
-	
+		int _pin;
+		uint8_t _activeHL;
+		unsigned lon _interval;
 	
 };
 
