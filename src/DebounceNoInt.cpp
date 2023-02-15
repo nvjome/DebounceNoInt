c7e88d7096b9c694bf959b851e8804a7f7d4167a
nvjome <94138462+nvjome@users.noreply.github.com>
2023-02-15 14:18:54 -0600

    Convert tabs to 4 spaces

--- src/DebounceNoInt.cpp
+++ src/DebounceNoInt.cpp
@@ -22,27 +22,27 @@ DebounceNoInt::~DebounceNoInt() {
 }
 
 /*
-	begin
+    begin
 
-	Initializes hardware related to object, along with some private attributes.
+    Initializes hardware related to object, along with some private attributes.
     Does no checking to determine if the desired pin is valid.
 */
 void DebounceNoInt::begin() {
-	pinMode(pin_, mode_);
+    pinMode(pin_, mode_);
 
-	// Time between pin reads is total debounce time divided by length of history,
-	// hardcoded here as 8. This could be changed, but 8 seems to be long enough,
-	// and fits in one uint_8 variable.
-	debounce_interval_us_ = debounce_time_us_ / 8;
+    // Time between pin reads is total debounce time divided by length of history,
+    // hardcoded here as 8. This could be changed, but 8 seems to be long enough,
+    // and fits in one uint_8 variable.
+    debounce_interval_us_ = debounce_time_us_ / 8;
 }
 
 /*
-	update
+    update
 
-	Performs the debounce algorithm. Must be called periodically and as often as
-	possible for best debouncing results. Returns true if the algorithm was run.
-	Return false if the debounce interval time was not met and the debounce state
-	was not updated.
+    Performs the debounce algorithm. Must be called periodically and as often as
+    possible for best debouncing results. Returns true if the algorithm was run.
+    Return false if the debounce interval time was not met and the debounce state
+    was not updated.
 */
 bool DebounceNoInt::update() {
     // if state was changed to an edge last update(), change to corresponding next state
@@ -53,46 +53,46 @@ bool DebounceNoInt::update() {
         state_ = LOW;
     }
     
-	unsigned long curr_debounce_micros = micros();
+    unsigned long curr_debounce_micros = micros();
 
-	// check micros time to take new input reading
-	if ((curr_debounce_micros - last_debounce_micros_) >= debounce_interval_us_) {
-		// reset previous micros time reading
-		last_debounce_micros_ = curr_debounce_micros;
-		// prepare history for new input reading
-		history_ <<= 1;
-		
-		// if input is high, set LSB to 1
-		// if input is low, set LSB to 0
-		if (digitalRead(pin_)) {
-			history_ |= 0x01;
-		} else {
-			history_ &= ~0x01;
-		}
+    // check micros time to take new input reading
+    if ((curr_debounce_micros - last_debounce_micros_) >= debounce_interval_us_) {
+        // reset previous micros time reading
+        last_debounce_micros_ = curr_debounce_micros;
+        // prepare history for new input reading
+        history_ <<= 1;
+        
+        // if input is high, set LSB to 1
+        // if input is low, set LSB to 0
+        if (digitalRead(pin_)) {
+            history_ |= 0x01;
+        } else {
+            history_ &= ~0x01;
+        }
 
-		// update debounce state based on input history
-    	switch (history_) {
-		case 0b10000000:
-			state_ = FALL;
-			break;
-		case 0b01111111:
-			state_ = RISE;
-			break;
-		default:
-			; // must be bounce, don't change state
-		}
-	}
+        // update debounce state based on input history
+        switch (history_) {
+        case 0b10000000:
+            state_ = FALL;
+            break;
+        case 0b01111111:
+            state_ = RISE;
+            break;
+        default:
+            ; // must be bounce, don't change state
+        }
+    }
 
-	return state_;
+    return state_;
 }
 
 /*
-	getState
+    getState
 
-	Returns the current debounced input state. Includes a noise state for when the
-	input is detected as bouncing. This DOES NOT run the debouncing algorithm, run() MUST
-	be called to update the input state!
+    Returns the current debounced input state. Includes a noise state for when the
+    input is detected as bouncing. This DOES NOT run the debouncing algorithm, run() MUST
+    be called to update the input state!
 */
 debounce_state DebounceNoInt::getState() {
-	return state_;
+    return state_;
 }
--- src/DebounceNoInt.h
+++ src/DebounceNoInt.h
@@ -6,21 +6,21 @@
 enum debounce_state {HIGH, LOW, RISE, FALL, NOISE};
 
 class DebounceNoInt {
-	public:
-		DebounceNoInt(int pin, int mode, unsigned long debounce_time_us);
-		virtual ~DebounceNoInt();
-		void begin();
-		bool update();
-		debounce_state getState();
-	
-	private:
-		int pin_;
-		int mode_;
-		unsigned long debounce_time_us_;
-		unsigned long debounce_interval_us_ = 0;
-		uint8_t history_ = 0;
-		debounce_state state_ = NOISE;	// safe default state
-		unsigned long last_debounce_micros_ = 0;
+    public:
+        DebounceNoInt(int pin, int mode, unsigned long debounce_time_us);
+        virtual ~DebounceNoInt();
+        void begin();
+        bool update();
+        debounce_state getState();
+    
+    private:
+        int pin_;
+        int mode_;
+        unsigned long debounce_time_us_;
+        unsigned long debounce_interval_us_ = 0;
+        uint8_t history_ = 0;
+        debounce_state state_ = NOISE;	// safe default state
+        unsigned long last_debounce_micros_ = 0;
 };
 
 #endif
