50273fabca120a75b2aa88c8654deff91269454f
nvjome <94138462+nvjome@users.noreply.github.com>
2023-02-15 13:33:45 -0600

    Update constructor

--- src/DebounceNoInt.h
+++ src/DebounceNoInt.h
@@ -5,23 +5,19 @@
 
 enum debounce_state {HIGH, LOW, RISE, FALL, NOISE};
 
-#define DEBOUNCE_ACTIVE_LOW 0x00
-#define DEBOUNCE_ACTIVE_HIGH 0x01
-
 class DebounceNoInt {
 	public:
-		DebounceNoInt(int pin, uint8_t active_high_low, unsigned long debounce_time_us, bool pullup);
+		DebounceNoInt(int pin, int mode, unsigned long debounce_time_us);
 		virtual ~DebounceNoInt();
 		void begin();
 		bool update();
 		debounce_state getState();
 	
 	private:
-		int _pin;
-		uint8_t _active_high_low;
-		unsigned long _debounce_time_us;
+		int pin_;
+		int mode_;
+		unsigned long debounce_time_us_;
 		unsigned long debounce_interval_us = 0;
-		bool _pullup;
 		uint8_t history = 0;
 		debounce_state _state = NOISE;	// safe default state
 		unsigned long last_debounce_micros = 0;
